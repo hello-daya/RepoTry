5c951da6e43dd379ca3eaa96dd9057d442f872b7
wintrade <dayabhat2@gmail.com>
2023-05-06 15:05:41 +0530

    Box program added

---
+++ AlgoCodes/box.py
@@ -0,0 +1,26 @@
+#Program to print a Box
+#
+import os
+os.system('cls')
+#
+i = 0
+j = 0
+print("Start")
+while i < 30:
+    print("=", end = " ")
+    i = i + 1
+i = 0
+print()
+while j < 10:
+    print("=", end = " ")
+    while i < 28:
+        print(" ", end = " ")
+        i = i + 1
+    print("=")
+    i = 0
+    j = j + 1
+while i < 30:
+    print("=", end = " ")
+    i = i + 1
+print()
+print("End")
